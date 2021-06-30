35e80dae6ad57d747faeb06440185d69dba57365
suke28 <sukesan178@icloud.com>
2021-06-30 20:52:37 +0900

    マイグレーションの実行

--- db/migrate/20210630112323_devise_create_users.rb
+++ db/migrate/20210630112323_devise_create_users.rb
@@ -6,6 +6,12 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
+      t.string :nickname, null: false
+      t.string :family_name, null: false
+      t.string :first_name, null: false
+      t.string :furigana_family_name, null: false
+      t.string :furigana_first_name, null: false
+      t.date :birthdate, null: false
 
       ## Recoverable
       t.string   :reset_password_token
