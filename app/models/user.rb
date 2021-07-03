e21e195f2d5ce22914c941f8145a9fd99ca5a1d4
suke28 <sukesan178@icloud.com>
2021-07-03 16:29:21 +0900

    バリデーションの作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname, presence: true
+  validates :family_name, presence: true
+  validates :first_name, presence: true
+  validates :furigana_family_name, presence: true
+  validates :furigana_first_name, presence: true
+  validates :birthdate, presence: true
+
+  has_many :items
+  has_many :purchases
 end
