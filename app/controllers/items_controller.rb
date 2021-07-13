b99cf3f29c99575132d3a30d3a710fad24b8eb3a
suke28 <sukesan178@icloud.com>
2021-07-14 00:02:07 +0900

    active_strageの追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,10 @@
 class ItemsController < ApplicationController
   def index
   end
+
+  private
+
+  def items_params
+    params.require(:items).permit(:image).merge(user_id: current_user.id)
+  end
 end
