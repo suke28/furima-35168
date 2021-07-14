c515b0dd598e1755a53d6e8c4af7a3a64d6bab15
suke28 <sukesan178@icloud.com>
2021-07-14 21:39:12 +0900

    ActiveHashの導入、バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,5 +3,14 @@ class Item < ApplicationRecord
 
   has_one_attached :image
 
-  validates :image, presence: true
+  validates :image, :name, :description, presence: true
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :day_to_ship
+  belongs_to :delivery_charge
+  belongs_to :delivery_source
+
+  validates :category_id, :condition_id, :day_to_ship_id, :delivery_charge_id, :delivery_source_id, numericality: { other_than: 1 , message: "can't be blank"} 
 end
