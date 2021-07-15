29b70ff7eae1ef1feef809097bf9170a881fcf0d
suke28 <sukesan178@icloud.com>
2021-07-15 23:26:16 +0900

    バリデーションの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,10 +1,18 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, {only: [:new]}
   def index
   end
 
-  private
+  def new
+    @item = Item.new
+  end
+  
+  def create
+    Item.create(items_params)
+  end
 
+  private
   def items_params
-    params.require(:items).permit(:image).merge(user_id: current_user.id)
+    params.require(:items).permit(:image, :name, :description, :category_id, :condition_id, :day_to_ship_id, :delivery_charge_id, :delivery_source_id,).merge(user_id: current_user.id)
   end
 end
