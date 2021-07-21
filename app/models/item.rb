cb8d9347a3aa8cb26a29805d1563a533f89e610c
suke28 <sukesan178@icloud.com>
2021-07-21 20:50:32 +0900

    単体テストコード記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -13,4 +13,5 @@ class Item < ApplicationRecord
   belongs_to :delivery_source
 
   validates :category_id, :condition_id, :day_to_ship_id, :delivery_charge_id, :delivery_source_id, numericality: { other_than: 1 , message: "can't be blank"} 
+  validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,69 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @user = FactoryBot.build(:user)
+    @item = FactoryBot.build(:item)
+  end
+
+  describe '商品出品機能' do
+    context '商品出品ができる時' do
+      it '全ての値が正しい場合は登録できる' do
+        expect(@item).to be_valid
+      end
+    end
+
+    context '商品出品ができない時' do
+      it '商品画像がないと出品できない' do
+        @item.image = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Image can't be blank")
+      end
+      it '商品名がないと出品できない' do
+        @item.name = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Name can't be blank")
+      end
+      it '商品の説明がないと出品できない' do
+        @item.description = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Description can't be blank")
+      end
+      it 'カテゴリーを選択しないと出品できない' do
+        @item.category_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Category can't be blank")
+      end
+      it '商品の状態を選択しないと出品できない' do
+        @item.condition_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Condition can't be blank")
+      end
+      it '配送料の負担を選択しないと出品できない' do
+        @item.delivery_charge_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Delivery charge can't be blank")
+      end
+      it '発送元の地域を選択しないと出品できない' do
+        @item.delivery_source_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Delivery source can't be blank")
+      end
+      it '発送までの日数を選択しないと出品できない' do
+        @item.day_to_ship_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Day to ship can't be blank")
+      end
+      it '価格が299円以下では出品できない' do
+        @item.price = Faker::Number.between(from: 0, to: 299)
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price must be greater than or equal to 300")
+      end
+      it '価格が10000000円以上では出品できない' do
+        @item.price = 10000000
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price must be less than or equal to 9999999")
+      end
+    end
+  end
 end
